03d5f6e2d3ba40e9f6fd6bf5e2b0864cf9842018
formateur1 <enseignant.formateur@protonmail.com>
2022-05-30 14:25:17 +0200

    Ajout de la fonction deleteStudent

--- src/main/java/com/inti/TD1Rest/controller/EtudiantController.java
+++ src/main/java/com/inti/TD1Rest/controller/EtudiantController.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.inti.TD1Rest.model.Etudiant;
@@ -51,6 +53,13 @@ public class EtudiantController
 		etudiantRepository.save(e1);
 		
 		return "The student : " + e1 + " has been updated";
+	}
+	
+	@DeleteMapping("/deleteStudent")
+	public String deleteStudent(@RequestParam("id") int id)
+	{
+		etudiantRepository.deleteById(id);
+		return "Student deleted";
 	}
 	
 
